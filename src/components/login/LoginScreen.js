100d64bd89894433788494dcaefce1fa540fdde6
Samuel Ramirez <samuel_ramirez@live.com.mx>
2021-08-29 18:01:32 -0500

    proteccion de rutas

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
 
+  const { dispatch } = useContext( AuthContext );
+
   const handleClick = () => {
+    const lastPath = localStorage.getItem('lastPath') || '/';
+    
+    dispatch({
+      type: types.login,
+      payload: {
+        name: 'Samuel'
+      }
+    });
+    
+    history.replace( lastPath );
     // history.push('/'); // agrega la ruta a la historia del navegador
-    history.replace('/'); // reemplaza una ruta previa por una nueva en la historia del navegador
+    // history.replace('/'); // reemplaza una ruta previa por una nueva en la historia del navegador
   }
 
   return (
